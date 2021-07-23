f2e4cc41a7de9005791644d3f3a86d73d4edabe4
paulroar <81697060+paulroar@users.noreply.github.com>
2021-07-23 18:14:16 +0100

    Add files via upload

---
+++ Exe2a.java
@@ -0,0 +1,44 @@
+package aula02;
+
+import java.util.Scanner;
+
+
+public class Exe2a {
+    
+    
+    public static void main(String[] args) {
+
+        int numero;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("insira um numero:");
+        numero = input.nextInt();
+
+        System.out.println("o numero foi: " + numero);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
